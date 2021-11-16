d60049949329ee7885b47f08f4f05d29756d5d89
ArmaanAS <armaanrsalam@gmail.com>
2021-11-16 09:23:34 +0000

    Update Module

--- src/api/UR_API.js
+++ src/api/UR_API.js
@@ -1,79 +1,41 @@
-import UROAuth from 'urban-rivals-oauth';
+import UROAuth from 'urban-rivals-oauth'
 import { config } from 'dotenv'
+import process from 'process'
 
 config();
 
-// app - key & secret is static
-// request token
-
-const auth = {
-  request: "https://www.urban-rivals.com/api/auth/request_token.php",
-  access: "http://www.urban-rivals.com/api/auth/access_token.php",
-  key: "eb70e322189e1bdea45ccea251ed06d805f34bcba",
-  secret: "a4303ab3b25f1d5b476e2d70df93b8a8",
-  api: "https://www.urban-rivals.com/api/",
-};
-
 const urApi = new UROAuth({
-  key: auth.key,
-  secret: auth.secret
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET,
 });
 
-// requestToken = {
-//   token: '18b5ee88c5c7399b401c1d5c9a02c4ee05f35d26d',
-//   secret: 'b654ec979bb82b13562032760e9af82d'
-// }
 
+export default async function callAPI(method, params) {
+  const { items, context } = await urApi.query(method, params)
 
-function query() {
-  console.log("querying");
-  urApi.getAccessToken()
-    .then(a => {
-      console.log("getAccessToken", a);
-
-      urApi.query('urc.getCharacters', {})
-        .then(a => {
-          console.log("query", a);
-        }).catch(err => {
-          console.error("query", err);
-        });
-
-    }).catch(err => {
-      console.error("getAccessToken", err);
-    });
+  return { items, context };
 }
 
-urApi.getRequestToken().then(a => {
-  console.log('getRequestToken', a);
+// Get initial request token 
+const requestToken = await urApi.getRequestToken();
+console.log('getRequestToken', requestToken);
 
-  const url = urApi.getAuthorizeUrl('https://localhost/');
-  console.log(url);
-});
+// Log URL to authorize your account
+const url = urApi.getAuthorizeUrl('https://localhost/');
+console.log(url);
 
 
-let stdin = process.openStdin();
 
-stdin.addListener("data", d => {
-  console.log("your input: " + d.toString());
+// Wait for enter input
+console.info("\nPress <ENTER> when ready\n");
+process.stdin.resume();
+await new Promise(res => process.stdin.once("data", res));
+process.stdin.end();
 
-  query();
-});
+// Get players accessToken, to make requests with
+await urApi.getAccessToken();
 
-let a, b;
-
-function callAPI(method, params) {
-  urApi.query(method, params)
-    .then(({
-      items,
-      context
-    }) => {
-      a = items;
-      b = context;
-      console.log(items, context);
-    }).catch(e => {
-      console.log(e);
-    });
-}
-callAPI("characters.getCharacters", {
+const result = await callAPI("characters.getCharacters", {
   charactersIDs: 123,
 });
+console.debug(result);
