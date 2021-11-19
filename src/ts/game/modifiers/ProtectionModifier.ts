11196307d66ac90c757363c80ab0ddf627f5dfa4
ArmaanAS <armaanrsalam@gmail.com>
2021-11-19 13:27:14 +0000

    Add Protection Cards ability variant

--- src/ts/game/modifiers/ProtectionModifier.ts
+++ src/ts/game/modifiers/ProtectionModifier.ts
@@ -18,29 +18,56 @@ export const ProtObject: { [key: string]: Prot } = {
 }
 
 export default class ProtectionModifier extends Modifier {
-  prot = Prot.POWER;
-  constructor(prot: Prot | string, et = EventTime.PRE3) {
+  prot: Prot;
+  both: boolean;
+  constructor(prot: Prot | string, both = false, et = EventTime.PRE3) {
     super();
 
     if (typeof prot == 'string')
       this.prot = ProtObject[prot.toUpperCase()]
     else
       this.prot = prot;
+
+    if (both)
+      console.log("Set both: " + both);
+    this.both = both;
     this.eventTime = et;
   }
 
+  setBoth(both: boolean) {
+    console.log("Set both: " + both);
+    this.both = both;
+
+    return this;
+  }
+
   apply(data: BattleData) {
-    switch (this.prot) {
-      // case Prot.POWER: data.card.power_.prot = true; break;
-      // case Prot.DAMAGE: data.card.damage_.prot = true; break;
-      // case Prot.ATTACK: data.card.attack_.prot = true; break;
-      // case Prot.ABILITY: data.card.ability_.prot = true; break;
-      // case Prot.BONUS: data.card.bonus_.prot = true; break;
-      case Prot.POWER: data.card.power.prot = true; break;
-      case Prot.DAMAGE: data.card.damage.prot = true; break;
-      case Prot.ATTACK: data.card.attack.prot = true; break;
-      case Prot.ABILITY: data.card.ability.prot = true; break;
-      case Prot.BONUS: data.card.bonus.prot = true; break;
+    if (this.both) {
+      switch (this.prot) {
+        case Prot.POWER:
+          data.oppCard.power.prot = true;
+          data.card.power.prot = true; break;
+        case Prot.DAMAGE:
+          data.oppCard.damage.prot = true;
+          data.card.damage.prot = true; break;
+        case Prot.ATTACK:
+          data.oppCard.attack.prot = true;
+          data.card.attack.prot = true; break;
+        case Prot.ABILITY:
+          data.oppCard.ability.prot = true;
+          data.card.ability.prot = true; break;
+        case Prot.BONUS:
+          data.oppCard.bonus.prot = true;
+          data.card.bonus.prot = true; break;
+      }
+    } else {
+      switch (this.prot) {
+        case Prot.POWER: data.card.power.prot = true; break;
+        case Prot.DAMAGE: data.card.damage.prot = true; break;
+        case Prot.ATTACK: data.card.attack.prot = true; break;
+        case Prot.ABILITY: data.card.ability.prot = true; break;
+        case Prot.BONUS: data.card.bonus.prot = true; break;
+      }
     }
   }
 }
